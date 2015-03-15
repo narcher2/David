60f25bed0040a2768f3e2e0a76d0e8fce304c66b
narcher2 <narcher2@gmu.edu>
2015-03-15 14:21:59 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,6 +67,8 @@ function create() {
     aliens.physicsBodyType = Phaser.Physics.ARCADE;
 
     //createAliens();
+    
+    game.time.events.repeat(Phaser.Timer.SECOND * 2, 10, createCar, this);
 
     //  The score
     scoreString = 'Score : ';
@@ -98,6 +100,27 @@ function create() {
     cursors = game.input.keyboard.createCursorKeys();
     fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     
+}
+
+function createCar() {
+
+
+            var alien = aliens.create(x * 48, y * 50, 'invader');
+            alien.anchor.setTo(0.5, 0.5);
+            alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
+            alien.play('fly');
+            alien.body.moves = false;
+
+
+    aliens.x = 100;
+    aliens.y = 50;
+
+    //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
+    var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+
+    //  When the tween loops it calls descend
+    tween.onLoop.add(descend, this);
+
 }
 
 function createAliens () {
