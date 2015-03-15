7a3a29ab1c1f0765ae11ab25acc3df006abde517
narcher2 <narcher2@gmu.edu>
2015-03-15 14:40:10 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -112,7 +112,7 @@ function createCar() {
     aliens.x = 100;
     aliens.y = 50;
     
-    alien.body.velocity.y = 200;
+    alien.body.velocity.y = -200;
 
     //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
     //var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
