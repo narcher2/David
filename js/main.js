c046d0f6d70709f2306e3236969b16e310ca97de
narcher2 <narcher2@gmu.edu>
2015-03-15 14:28:41 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -103,24 +103,26 @@ function create() {
 }
 
 function createCar() {
-
-
+    for (var y = 0; y < 4; y++)
+    {
+        for (var x = 0; x < 10; x++)
+        {
             var alien = aliens.create(x * 48, y * 50, 'invader');
             alien.anchor.setTo(0.5, 0.5);
             alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
             alien.play('fly');
             alien.body.moves = false;
+        }
+    }
 
-
-    //aliens.x = 100;
-    //aliens.y = 50;
+    aliens.x = 100;
+    aliens.y = 50;
 
     //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
     var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
 
     //  When the tween loops it calls descend
     tween.onLoop.add(descend, this);
-
 }
 
 function createAliens () {
