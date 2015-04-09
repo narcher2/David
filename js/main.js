0e6ca1c4137be59f879092cceede8fbf4dd88627
narcher2 <narcher2@gmu.edu>
2015-04-09 10:37:44 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -109,7 +109,7 @@ function update() {
     if (game.time.now > turnTimer && game.time.now > 500)
     {
         turnTimer = game.time.now + 2000;
-        //c.body.velocity.set(game.rnd.integerInRange(-50, 50), game.rnd.integerInRange(-50, 50));
+        c.body.velocity.set(game.rnd.integerInRange(-50, 50), game.rnd.integerInRange(-50, 50));
         //group.body.velocity.x = 0;
         //group.body.velocity.y = 0;
     }
