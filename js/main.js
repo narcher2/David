32a5ba6257fc3202324ab4db6de4d5cf1348f1e0
narcher2 <narcher2@gmu.edu>
2015-04-09 12:54:10 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ function create() {
 
     sprite = game.add.sprite(500, 1000, 'player');
     sprite.anchor.set(0.5);
-    //sprite.animations.add('sling', [0, 1, 2, 3, 4, 5, 6, 7, 8], 20, true);
+    sprite.animations.add('sling', [0, 1, 2, 3, 4, 5, 6, 7, 8], 20, true);
 
     // game.physics.arcade.sortDirection = Phaser.Physics.Arcade.TOP_BOTTOM;
     game.physics.arcade.sortDirection = Phaser.Physics.Arcade.BOTTOM_TOP;
@@ -169,10 +169,10 @@ function update() {
         sprite.body.rotation = rotSum/buttonCount;
         game.physics.arcade.velocityFromRotation(sprite.rotation, 200, sprite.body.velocity);
     }
-    /*if (fireButton.isDown)
+    (fireButton.isDown)
     {
             sprite.play('sling');
-    }*/
+    }
     
     if (game.time.now > turnTimer && game.time.now > 500)
     {
